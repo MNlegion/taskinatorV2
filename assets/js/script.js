b86407651b320fa94da7a33cede55ff3a08a5628
MNlegion <rkreuser30@gmail.com>
2024-01-20 11:08:57 -0500

    user input form fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
-const formEl = document.querySelector("#task-form");
-const tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-const createTaskHandler = function(event) {
+var createTaskHandler = function(event) {
     event.preventDefault();
 
-    const listItemEl = document.createElement("li");
+    // get form values from user input
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskDueDateInput = document.querySelector("input[name='due-date']").value;
+    
+    // create list item
+    var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>" + "<br/>" + "<span class='task-due-date'>" + taskDueDateInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
 formEl.addEventListener("submit", createTaskHandler);
