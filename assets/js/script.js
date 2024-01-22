2c92ff1905501ab45f0e40f598cc033e568d2bbd
MNlegion <rkreuser30@gmail.com>
2024-01-21 20:16:51 -0500

    check form for input validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskDueDateInput = document.querySelector("input[name='due-date']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
